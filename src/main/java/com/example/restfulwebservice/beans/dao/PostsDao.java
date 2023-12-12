fa763e7a9f9bd631ab5aeafa95d710a127132ed1
Venkata Rajesh Alapati <valapati@uncc.edu>
2023-12-12 18:41:19 -0500

    Refactor and Delete Post Implementation

--- src/main/java/com/example/restfulwebservice/beans/dao/PostsDao.java
+++ src/main/java/com/example/restfulwebservice/beans/dao/PostsDao.java
@@ -2,6 +2,15 @@ package com.example.restfulwebservice.beans.dao;
 
 import com.example.restfulwebservice.beans.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface PostsDao extends JpaRepository<Posts, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM POSTS WHERE ID = :postid AND USER_ID = :userid", nativeQuery = true)
+    void deletePost(@Param("userid") int userid, @Param("postid") int postid);
 }
--- src/main/java/com/example/restfulwebservice/controllers/UsersController.java
+++ src/main/java/com/example/restfulwebservice/controllers/UsersController.java
@@ -1,26 +1,18 @@
 package com.example.restfulwebservice.controllers;
 
+import com.example.restfulwebservice.beans.Posts;
+import com.example.restfulwebservice.beans.Users;
+import com.example.restfulwebservice.beans.dao.PostsDao;
+import com.example.restfulwebservice.beans.dao.UsersDao;
+import com.example.restfulwebservice.exceptions.UserNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import java.net.URI;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.NoSuchElementException;
-
-import com.example.restfulwebservice.beans.Posts;
-import com.example.restfulwebservice.beans.dao.PostsDao;
-import jakarta.validation.Valid;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.example.restfulwebservice.beans.Users;
-import com.example.restfulwebservice.beans.dao.UsersDao;
-import com.example.restfulwebservice.exceptions.UserNotFoundException;
 
 @RestController
 public class UsersController {
@@ -32,6 +24,11 @@ public class UsersController {
 		super();
 		this.userdao = userdao;
 		this.postsDao = postsDao;
+	}
+
+	@GetMapping("/")
+	public String welcome(){
+		return "Documentation available at /swagger-ui.html \n Database at /h2-console";
 	}
 
 	@GetMapping("/users")
@@ -87,5 +84,10 @@ public class UsersController {
 	@DeleteMapping("/users/{id}")
 	public void deleteUser(@PathVariable int id) {
 		userdao.deleteById(id);
+	}
+
+	@DeleteMapping("/users/{userid}/posts/{postid}")
+	public void deletePost(@PathVariable int userid, @PathVariable int postid) {
+		postsDao.deletePost(userid, postid);
 	}
 }
